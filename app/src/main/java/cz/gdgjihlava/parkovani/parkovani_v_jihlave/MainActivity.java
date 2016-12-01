a4756cbc913b44aa89b3419efb2f9a0fd10a895e
Miroslav Matocha <matocham254@gmail.com>
2016-12-01 19:10:25 +0100

    added activity_send

--- app/src/main/java/cz/gdgjihlava/parkovani/parkovani_v_jihlave/MainActivity.java
+++ app/src/main/java/cz/gdgjihlava/parkovani/parkovani_v_jihlave/MainActivity.java
@@ -8,8 +8,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+
     }
 
     @Override
