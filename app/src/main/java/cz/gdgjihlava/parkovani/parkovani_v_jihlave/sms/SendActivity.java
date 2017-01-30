429fe526a573ffe117a0731fb9982f388bf56fa9
Horm <horak@horm.cz>
2017-01-30 17:43:25 +0100

    Add menu item to save SPZ

--- app/src/main/java/cz/gdgjihlava/parkovani/parkovani_v_jihlave/sms/SendActivity.java
+++ app/src/main/java/cz/gdgjihlava/parkovani/parkovani_v_jihlave/sms/SendActivity.java
@@ -1,11 +1,14 @@
 package cz.gdgjihlava.parkovani.parkovani_v_jihlave.sms;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +17,7 @@ import android.Manifest;
 
 import cz.gdgjihlava.parkovani.parkovani_v_jihlave.R;
 import cz.gdgjihlava.parkovani.parkovani_v_jihlave.SMS;
+import cz.gdgjihlava.parkovani.parkovani_v_jihlave.SaveSPZ;
 import cz.gdgjihlava.parkovani.parkovani_v_jihlave.notifications.OngoingNotification;
 
 
@@ -56,6 +60,30 @@ public class SendActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent spzSettingsIntent = new Intent(this, SaveSPZ.class);
+            startActivity(spzSettingsIntent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void sendSMS() {
