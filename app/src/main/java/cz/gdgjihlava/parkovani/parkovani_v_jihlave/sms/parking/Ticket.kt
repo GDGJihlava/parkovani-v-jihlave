bb2a0867f73c73df8ad250a46ac5cff6f37524ae
Jan Černý <j.cerny.zdar@gmail.com>
2017-06-28 09:38:44 +0200

    Add iOS app layout

--- app/src/main/java/cz/gdgjihlava/parkovani/parkovani_v_jihlave/sms/parking/Ticket.kt
+++
@@ -1,7 +0,0 @@
-package cz.gdgjihlava.parkovani.parkovani_v_jihlave.sms.parking
-
-/**
- * Created by horm on 21.6.17.
- */
-
-class Ticket(val parkingLotName: String, val zoneCode: Int, val price: Int, val duration: Int)
--- app/src/main/java/cz/gdgjihlava/parkovani/parkovani_v_jihlave/sms/parking/Zone.kt
+++
@@ -1,3 +0,0 @@
-package cz.gdgjihlava.parkovani.parkovani_v_jihlave.sms.parking
-
-data class Zone(val code: Int, val ticketDurationInMinutes: Int, val ticketPriceInCZK: Int)
